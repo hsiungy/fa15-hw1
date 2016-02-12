d0f2e7c250fae7ee52d3e5a2f8f5acedb3423dee
Ya-An Hsiung <ya-anhsiung@berkeley.edu>
2016-02-12 14:41:50 -0800

    foobar
    finished q4

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,29 @@
 class Foobar
-  # Q4 CODE HERE
+  def Foobar.baz(a)
+  	b = []
+  	c = []
+  	# change each string into an integer
+  	a.each do |i|
+  		j = i.to_i
+  		b << j
+  	end
+
+  	# add two to each number
+  	b.each do |i|
+  		j = i + 2
+  		c << j
+  	end
+
+  	# keep the even numbers only
+  	c.delete_if {|i| i%2 != 0}
+
+  	# take out any duplicates
+  	d = c.uniq
+
+  	# reject resulting numbers > 10
+  	d.delete_if {|i| i > 10}
+
+  	# return sum of resulting array
+  	return d.inject(0, &:+)
+  end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,13 +1,19 @@
 def squared_sum(a, b)
-  # Q1 CODE HERE
+  sum = a + b
+  return sum ** 2
 end
 
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
+  b = []
+  a.each do |i|
+    j = i + 1
+    b << j
+  end
+  return b.sort
 end
 
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
+  return first_name.concat(" " + last_name)
 end
 
 def blockin_time(a)
